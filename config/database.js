d28b9c511829616fb4e1f9a4713d1063e60cc5cc
liturgy bureau <chris.karayannidis@protonmail.ch>
2022-09-24 13:05:18 +0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
-    database: 'mongodb+srv://liturgy:litworker369@liturgy0-opnag.mongodb.net/users?retryWrites=true&w=majority', // the users database
-    allobjdb: 'mongodb+srv://liturgy:litworker369@liturgy0-opnag.mongodb.net/allObjects?retryWrites=true&w=majority', // the objects database
+    database: 'mongodb+srv://xxxxxx:xxxxxx@xxxxxx0-xxxxxx.mongodb.net/users?retryWrites=true&w=majority', // the users database
+    allobjdb: 'mongodb+srv://xxxxxx:xxxxxx@xxxxxx0-xxxxxx.mongodb.net/allObjects?retryWrites=true&w=majority', // the objects database
     secret: 'xxxxxxx', // used for user auth token
     forgotpwdkey: 'xxxxxxx' // used for link token to be send
 }
